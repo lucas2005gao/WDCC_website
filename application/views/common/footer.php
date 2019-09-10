dafc37e5319de44d63795c0560d0fc6e32033534
Lucas Gao <lucas2005gao@gmail.com>
2019-09-10 16:48:26 +1200

    Added css in header and js in footer

---
+++ application/views/common/footer.php
@@ -0,0 +1,3 @@
+<script src="<?=site_url()?>/assets/js/jquery-3.4.1.min.js"></script>
+<script src="<?=site_url()?>/assets/js/bootstrap.js"></script>
+</body>
---
+++ application/views/common/header.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/assets/css/bootstrap.css">
+</head>
+
+<body>
+	
