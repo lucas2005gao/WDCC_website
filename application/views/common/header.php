2b6d53fef896d4a565826648c42287720a7574b0
Martin <martintiangco@gmail.com>
2019-09-21 16:01:41 +1200

    Added navigation bar

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -5,5 +5,22 @@
 	<link rel="stylesheet" href="/assets/css/bootstrap.css">
 </head>
 
+<ul class="nav nav-tabs">
+	<li class="col-xs-12 col-sm-6 col-md-6 col-lg-9">
+		<a href="#">
+			<img src="/assets/coming-soon.jpg" style="width:50px;height:50px;">
+		</a>
+	</li>
+	<li class="nav-item col-0.5">
+		<a class="nav-link active" href="#">Home</a>
+	</li>
+	<li class="nav-item col-0.5">
+		<a class="nav-link" href="#">About Us</a>
+	</li>
+	<li class="nav-item col-0.5">
+		<a class="nav-link" href="#">Contact</a>
+	</li>
+</ul>
+
 <body>
 	
