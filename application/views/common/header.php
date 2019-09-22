6032c86f041a1bfd6c47fcc3b99dc1043f50732f
zyan225 <ziwei.yang116@gmail.com>
2019-09-22 15:56:55 +1200

    footer is on the bottom of the page, but the footer contains overflow problem

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,26 +1,29 @@
 <!doctype html>
-<html>
+<html style="height:100%; margin:0; padding:0">
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.css">
 </head>
 
+<div id="container" style="min-height:100%; position:relative">
+  <div id="main" style="overflow:auto; padding-bottom: 300px">
+
 <ul class="nav nav-tabs">
-	<li class="col-xs-12 col-sm-6 col-md-6 col-lg-9">
-		<a href="#">
-			<img src="/assets/coming-soon.jpg" style="width:50px;height:50px;">
-		</a>
-	</li>
-	<li class="nav-item col-0.5">
-		<a class="nav-link active" href="#">Home</a>
-	</li>
-	<li class="nav-item col-0.5">
-		<a class="nav-link" href="#">About Us</a>
-	</li>
-	<li class="nav-item col-0.5">
-		<a class="nav-link" href="#">Contact</a>
-	</li>
+      <li class="col-xs-12 col-sm-6 col-md-6 col-lg-9">
+        <a href="#">
+          <img src="/assets/coming-soon.jpg" style="width:50px;height:50px;">
+        </a>
+      </li>
+      <li class="nav-item col-0.5">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item col-0.5">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item col-0.5">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
 </ul>
 
-<body style="min-height: 100%">
+<body style="height: 100%">
 	
