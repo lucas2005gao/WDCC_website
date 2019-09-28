d2000cf0b53521bd8521357b1e83b06c0c27c012
Kevin Ge <gjw1047340859@gmail.com>
2019-09-28 13:20:26 +1200

    Basic Profile foudation is added

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_wdcc');
+		$this->load->view('homePage');
 	}
 }
--- application/views/homePage.php
+++ application/views/homePage.php
@@ -20,7 +20,118 @@
 <!-- =============================================================== -->
 <!-- ======================== KEVIN ======================= -->
 <!-- =============================================================== -->
-<p>type code here</p>
+<!-- <div class="mx-auto"> -->
+	<!-- <marquee>  -->
+
+
+    
+<!--  <pre class="pre-x-scrollable">
+ -->
+ <!-- container-fluid-->
+<!--  <div class="container"> -->
+ 	 <!-- <div class="pre-x-scrollable"> -->
+
+<!-- <div class="row">
+ -->
+
+<div class="container-fluid">  
+<div>
+<h1 class="text-center leader"> Member Profile </h1>
+</div>
+
+
+
+ <div class="col d-flex justify-content-center">
+
+ <div class="card-deck">
+
+
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+<div class="card-body p-3 mb-2 bg-light text-dark">Lucas Gao
+  
+  	
+		<h5>President</h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+</div>
+
+
+
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+
+  <div class="card-body p-3 mb-2 bg-light text-dark">Will Zhang
+  	
+		<h5>Vice President</h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+ </div>
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+  <div class="card-body p-3 mb-2 bg-light text-dark">Martin
+  	
+		<h5>Secretary      </h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+</div>
+
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+  <div class="card-body p-3 mb-2 bg-light text-dark">Kevin Ge
+  	
+		<h5>Treasurer</h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+</div>
+
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+  <div class="card-body p-3 mb-2 bg-light text-dark">Emily Yang
+  	
+		<h5>Leader of xxx </h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+</div>
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+  <div class="card-body p-3 mb-2 bg-light text-dark">Yujia Wu
+  	
+		<h5>Leader of xxx </h5>
+		            
+		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+		<p>content of person</p>
+		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+</div>
+
+<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
+  
+
+
+<!-- </marquee> 
+ -->
+
+<!-- </div>  -->
+
+</div>
+
+</div>
+</div>
+<!-- </div>
+ -->
 <!-- =============================================================== -->
 
 
