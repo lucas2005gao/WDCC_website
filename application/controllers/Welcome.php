c2d0da50c390bc8357d58e371e7b727d3094ca67
Lucas Gao <lucas2005gao@gmail.com>
2019-10-04 16:18:40 +1300

    Added contact us page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,4 +30,8 @@ class Welcome extends CI_Controller {
 	public function loadAboutUs(){
 		$this->load->view('aboutUsPage');
 	}
+
+	public function loadContactUs(){
+		$this->load->view('contactUsPage');
+	}
 }
