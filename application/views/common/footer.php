1507d757e723a6687e0d45612ff8943d1875dc40
zyan225 <ziwei.yang116@gmail.com>
2019-10-05 15:54:10 +1300

    add comment in the footer about override the shadow class

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -60,6 +60,7 @@
 <script src="<?=base_url()?>assets/js/jquery-3.4.1.min.js"></script>
 <script src="<?=base_url()?>assets/js/bootstrap.js"></script>
 
+<!-- after passing this style, all code that uses shadow class will be override by this-->
 <style>
     .shadow{
         box-shadow:none !important;
