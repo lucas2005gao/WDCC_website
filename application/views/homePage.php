2ea14f010f6aa64b6c6be259ca522e27fae08061
Kevin Ge <gjw1047340859@gmail.com>
2019-10-12 13:55:21 +1300

    responsive issue solved

--- application/views/homePage.php
+++ application/views/homePage.php
@@ -1,118 +1,102 @@
-<!doctype html>
-<html style="height:100%; margin:0; padding:0">
-<head>
-  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<?php 
+  $this->load->view("/common/header");
+?>
+    
+<!-- =============================================================== -->
+<!-- ======================== MARTIN ======================= -->
+<!-- =============================================================== -->
+<p>type code here</p>
+<!-- =============================================================== -->
+<!-- <section calss="slider">
+	<ul class="slides">
+      <li>
+        <img src="https://i1.wp.com/expresstricks.com/wp-content/uploads/2019/04/beautiful-picture-1.jpg?fit=1500%2C1000"> 
+        <div class="caption center-align">
+          <h3>Welcome to WebDevelopment&Consulting Club!</h3>
+          <h5 class="light grey-text text-lighten-3 hile-on small-only">President is really hanssome</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://s7img.ftdi.com/is/image/ProvideCommerce/PF_18_R037_ROSE_SUMMER_V1_ALT1_PF_SITE_PLP_BANNER?&$proflowers-hero-banner-lv$"> 
+        <div class="caption left-align">
+          <h3>Learn webDevelopment easily!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://iso.500px.com/wp-content/uploads/2016/04/stock-photo-150595123.jpg"> 
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      
+    </ul>
+	
+</section>
 
-  <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.css">
-  <link rel="stylesheet" href="<?=base_url()?>assets/css/style.css">
 
-</head>
-<body style="height: 100%">
-  
-  <!--PRELOADER-->
-  <div id="preloader">
-    <p> Loading... </p>
-      <div class="spinner-border" style="width: 10rem; height: 10rem;" role="status">
-        <span class="sr-only">Loading...</span>
+<script src="https://kit.fontawesome.com/e1573a2b30.js" crossorigin="anonymous"></script>
+
+<script>
+		//slider
+	/*const slider = document.querySelector('.slider');
+	M.Slider.init(slider,{
+		indicators: flase,
+		height: 500,
+		transition: 500,
+		interval: 6000
+	});*/
+	  $(document).ready(function(){
+    $('.slider').slider();
+  });
+
+</script> -->
+
+
+
+
+
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://i1.wp.com/pna.co.za/blog/wp-content/uploads/2017/08/Blog_3.png?fit=750%2C375&ssl=1" alt="..." height="400" width="100%">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
       </div>
-  </div>
-  <!--/.PRELOADER END-->
-
-<div id="container" style="min-height:100%; position:relative">
-  <div id="main" style="overflow:auto; padding-bottom: 300px">
-
-
-<!-- ================================================================================= -->
-<!-- ============================ carousel and Nav Bar together======================= -->
-<div>
-  <style>
-    .un {
-      display: inline-block;
-      color: rgba(255,255,255,0.8)
-    }
-    .un:after {
-      content: '';
-      width: 0px;
-      height: 2px;
-      display: block;
-      background: white;
-      color: rgba(255,255,255,1);
-      -moz-transition: all 1s ease;
-      -webkit-transition: all 1s ease;
-      transition: all 1s ease;
-    }
-    .un:hover:after {
-      width: 100%;
-      color: rgba(255,255,255,1);
-    }
-    .font {
-      font-size: 16px;
-    }
-  </style>
-  <nav class="navbar navbar-expand-sm navbar-light" style="z-index:99">
-    <a class="navbar-brand" style="color:white" href="#">
-      <a class="col-lg-9 col-xs-3" href="#">
-            <img src="/assets/Image-Coming-Soon.png" style="width:50px;height:50px;">
-          </a>
+      <div class="carousel-item">
+        <img src="https://i.kinja-img.com/gawker-media/image/upload/s--uGlCJcn5--/c_scale,f_auto,fl_progressive,q_80,w_800/zskle8pr6kignbkaz06j.jpg" alt="..." height="400" width="100%">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://mymodernmet.com/wp/wp-content/uploads/archive/oe-oqDrb4nkmDH0vA90Z_PaperSpiral5.jpg" alt="..." height="400" width="100%">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
     </a>
-    <div class="collapse navbar-collapse col-lg-3 col-xs-3">
-      <ul class="navbar-nav">
-        <li class="nav-item un">
-          <a class="nav-link" style="color:rgba(255,255,255,0.8)" href="#">Home</a>
-        </li>
-        <li class="nav-item un">
-          <a class="nav-link" style="color:rgba(255,255,255,0.8)" href="#">About Us</a>
-        </li>
-        <li class="nav-item un">
-          <a class="nav-link" style="color:rgba(255,255,255,0.8)" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-  <div class="bd-example" style="margin-top: -100px">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner position-carousel">
-        <div class="carousel-item active">
-          <img src="https://i1.wp.com/pna.co.za/blog/wp-content/uploads/2017/08/Blog_3.png?fit=750%2C375&ssl=1" alt="..." height="400" width="100%">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </div>
-        </div>
-        <div class="carousel-item position-carousel">
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/s--uGlCJcn5--/c_scale,f_auto,fl_progressive,q_80,w_800/zskle8pr6kignbkaz06j.jpg" alt="..." height="400" width="100%">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-        </div>
-        <div class="carousel-item position-carousel">
-          <img src="https://mymodernmet.com/wp/wp-content/uploads/archive/oe-oqDrb4nkmDH0vA90Z_PaperSpiral5.jpg" alt="..." height="400" width="100%">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </div>
-<!-- ================================================================================= -->
 
 
 <!-- =============================================================== -->
@@ -125,19 +109,19 @@
       <h2 style="text-align: center;">ABOUT US</h2> 
    
       <!-- detail explanation  -->
-      <p style="text-align:center; padding-right:30%; padding-left:30%">XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXX XXXXXXX XXXXXXXXXX</p>
+      <p style="text-align:center">XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXX XXXXXXX XXXXXXXXXX</p>
     
     </div>
 
     <style>
-      .hoverButton span {
+      .button span {
         cursor: pointer;
         display: inline-block;
         position: relative;
         transition: 0.5s;
       }
 
-      .hoverButton span:after {
+      .button span:after {
         content: '\00bb';
         position: absolute;
         opacity: 0;
@@ -146,46 +130,74 @@
         transition: 0.5s;
       }
 
-      .hoverButton:hover span {
+      .button:hover span {
         padding-right: 25px;
       }
 
-      .hoverButton:hover span:after {
+      .button:hover span:after {
         opacity: 1;
         right: 0;
       }
   </style>
-  <div class="row" style="padding-left: 30px; padding-right:30px; padding-top:50px; padding-bottom: 80px">
-    <div class="col" style="text-align:center">
+
+
+
+  <div class="container-fluid">
+
+ <!--  <div class="row text-center equal-height-columns" style="padding-left: 30px; padding-right:30px; padding-top:50px; padding-bottom: 80px"> -->
+ <div class="row" style="padding-left: 30px; padding-right:30px; padding-top:50px; padding-bottom: 80px">
+
+    <div class="col-lg-3" style="text-align:center">
+      <div class="equal-column-content">
       <img src="https://image.flaticon.com/icons/svg/1256/1256650.svg" alt="team icon" style="width:50px;height:50px;">
       <h4> Our Team </h4>
-      <p style="min-height:40%">Click the button below to find out more of our team.</p>
-      <button type="button" class="hoverButton btn btn-secondary"><span>Learn More</span></button>
+      <p>Click the button below to find out more of our team.</p>
+      <button type="button" class="button btn btn-secondary"><span>Learn More</span></button>
     </div>
+</div>
 
-    <div class="col" style="text-align:center">
+
+    <div class="col-lg-3" style="text-align:center">
+
+       <div class="equal-column-content">
+
       <img src="https://image.flaticon.com/icons/svg/1517/1517470.svg" alt="event icon" style="width:50px;height:50px;">
       <h4> Our Event </h4>
-      <p style="min-height:40%"> Click the button below to checkout more of our amazing events!</p>
-      <button type="button" class="hoverButton btn btn-secondary"><span>Learn More</span></button>
+      <p> Click the button below to checkout more of our amazing events!</p>
+      <button type="button" class="button btn btn-secondary"><span>Learn More</span></button>
     </div>
 
-    <div class="col" style="text-align:center">
+  </div>
+
+    <div class="col-lg-3" style="text-align:center">
+
+ <div class="equal-column-content">
+
       <img src="https://image.flaticon.com/icons/svg/1441/1441932.svg" alt="sponsor icon" style="width:50px;height:50px;">
       <h4> Our Sponsor </h4>
-      <p style="min-height:40%"> discription of the team </p>
-      <button type="button" class="hoverButton btn btn-secondary"><span>Learn More</span></button>
+      <p> discription of the team </p>
+      <button type="button" class="button btn btn-secondary"><span>Learn More</span></button>
     </div>
 
-    <div class="col" style="text-align:center">
+</div>
+
+    <div class="col-lg-3" style="text-align:center">
+
+       <div class="equal-column-content">
+
       <img src="https://image.flaticon.com/icons/svg/1894/1894482.svg" alt="membership icon" style="width:50px;height:50px;">
       <h4> Our Membership </h4>
-      <p style="min-height:40%"> discription of the team </p>
-      <button type="button" class="hoverButton btn btn-secondary"><span>Learn More</span></button>
+      <p> discription of the team </p>
+      <button type="button" class="button btn btn-secondary"><span>Learn More</span></button>
     </div>
+
+</div>
+
   </div>
 
 </div>
+
+
 <!-- =============================================================== -->
 
 
@@ -208,102 +220,98 @@
 <!-- <div class="row">
  -->
 
-<div class="container-fluid">  
-<div>
+ <div>
 <h1 class="text-center leader"> Member Profile </h1>
 </div>
 
 
 
- <div class="col d-flex justify-content-center">
-
- <div class="card-deck">
 
 
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-<div class="card-body p-3 mb-2 bg-light text-dark">Lucas Gao
-  
-  	
-		<h5>President</h5>
+<!-- <div class="col d-flex justify-content-center"> -->
+
+<div class="container-fluid p-3 mb-2 bg-light text-dark">
+
+		
+		
+
+	<div class="row">
+
+		<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Lucas Gao
+
+			<h5>President</h5>
 		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class=" rounded-circle" alt="loading" width="100" height="100"> 
 
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+		</div>
+
+
+
+		<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Will Zhang
+
+			<h5>Vice President</h5>
+		            
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+		</div>
+
+
+		<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Martin
+
+			<h5>Secretary</h5>
+		            
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+
+
+		</div>
+
+<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Kevin Ge
+
+			<h5>Treasurer</h5>
+		            
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
 </div>
 
 
+<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Emily Yang
 
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-
-  <div class="card-body p-3 mb-2 bg-light text-dark">Will Zhang
-  	
-		<h5>Vice President</h5>
+			<h5>Leader of xxx</h5>
 		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
 
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+
+
+</div>
+
+
+<div class="col-lg-2 p-3 mb-2 bg-light text-dark text-center">Yujia Wu
+
+			<h5>Leader of xxx</h5>
+		            
+			<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
+
+			<p>content of person</p>
+			<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
+
+</div>
+
+
  </div>
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-  <div class="card-body p-3 mb-2 bg-light text-dark">Martin
-  	
-		<h5>Secretary      </h5>
-		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
-
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
-
-</div>
-
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-  <div class="card-body p-3 mb-2 bg-light text-dark">Kevin Ge
-  	
-		<h5>Treasurer</h5>
-		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
-
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
-
-</div>
-
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-  <div class="card-body p-3 mb-2 bg-light text-dark">Emily Yang
-  	
-		<h5>Leader of xxx </h5>
-		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
-
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
-
-</div>
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-  <div class="card-body p-3 mb-2 bg-light text-dark">Yujia Wu
-  	
-		<h5>Leader of xxx </h5>
-		            
-		<img src="http://img.sj33.cn/uploads/allimg/201401/7-140126231522E0.jpg" class="rounded-circle" alt="loading" width="100" height="100"> 
-
-		<p>content of person</p>
-		<p><button class="btn btn-Info p-3 mb-2 bg-light text-dark">Contact</button></p>
-</div>
-
-<!-- <div class="col-lg-2 p-3 mb-2 bg-light text-dark"> -->
-  
-
-
-<!-- </marquee> 
- -->
-
-<!-- </div>  -->
-
-</div>
-
-</div>
-</div>
 <!-- </div>
  -->
 <!-- =============================================================== -->
@@ -322,6 +330,7 @@
 <!-- =============================================================== -->
 <p>type code here</p>
 <!-- =============================================================== -->
+
 
 
 <?php 
