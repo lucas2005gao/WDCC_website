d2ce37b5ad616aa5d32ebdb1e76cdcfecc7ecd6f
ywu666 <ywu660@aucklanduni.ac.nz>
2019-11-09 15:34:03 +1300

    Fix error for smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,29 +5,3 @@
                 $("#status").fadeOut(); // will first fade out the loading animation
                 $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
         });
-
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
